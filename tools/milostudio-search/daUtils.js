4519e34dcab89a2443001386eecfe3529ac6f9c6
Manasvi Agrawal <maagrawal@Manasvis-MacBook-Pro.local>
2025-02-05 23:30:05 +0530

    minor changes

--- tools/milostudio-search/daUtils.js
+++ tools/milostudio-search/daUtils.js
@@ -4,7 +4,7 @@ import DA_SDK from 'https://da.live/nx/utils/sdk.js';
 (async () => {
   try {
     const { context: { repo }, token } = await DA_SDK;
-    window.location.href = `./search/ms-search.html?tenant=${repo}&token=${token}`;
+    window.location.replace(`./search/ms-search.html?tenant=${repo}&token=${token}`);
   } catch (error) {
     console.error('Error initializing DA_SDK:', error);
   }
