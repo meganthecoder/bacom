9b4dad70d13c6b33b32918d21f21922d19d534b4
Manasvi Agrawal <maagrawal@Manasvis-MacBook-Pro.local>
2025-02-14 14:40:12 +0530

    passing path as a query parameter

--- tools/milostudio-search/daUtils.js
+++ tools/milostudio-search/daUtils.js
@@ -3,8 +3,10 @@ import DA_SDK from 'https://da.live/nx/utils/sdk.js';
 
 (async () => {
   try {
-    const { context: { repo }, token } = await DA_SDK;
-    window.location.replace(`https://milostudio--milo--adobecom.aem.page/tools/search/ms-search.html?tenant=${repo}&token=${token}`);
+    const { context, token } = await DA_SDK;
+    const { repo, path } = context;
+    const redirectPath = path.split('/').pop();
+    window.location.replace(`https://milostudio--milo--adobecom.aem.page/tools/da-apps/ms-apps.html?path=${redirectPath}&tenant=${repo}&token=${token}`);
   } catch (error) {
     console.error('Error initializing DA_SDK:', error);
   }
